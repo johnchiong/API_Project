b121ad7567cc584a56d3c3e806b8ac26942df55d
johnchiong <152459351+johnchiong@users.noreply.github.com>
2024-05-23 21:02:46 +0800

    Update Api.py

--- Api.py
+++ Api.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+import flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MySQL_HOST"] = "localhost"
+app.config["MySQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MSQL_DB"] = "Company"
+
+app.config['MSQL_CURSOCLASS'] = "DictCursor"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/department", methods=["GET"])
+def get_actors():
+    cur = mysql.connection.cursor()
+    query = """
+    select * from department
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close
+
+    return make_response(jsonify(data), 200)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
