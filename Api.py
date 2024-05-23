95e690cb3af01c0cf801c3fe5ef3f65d3efa154a
johnchiong <152459351+johnchiong@users.noreply.github.com>
2024-05-23 23:13:46 +0800

    updated

--- Api.py
+++ Api.py
@@ -2,12 +2,12 @@ from flask import Flask, make_response, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config["MySQL_HOST"] = "localhost"
-app.config["MySQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MSQL_DB"] = "Company"
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "Lenovo"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "Company"
 
-app.config['MSQL_CURSOCLASS'] = "DictCursor"
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 mysql = MySQL(app)
 
@@ -19,14 +19,23 @@ def hello_world():
 def get_employee():
     cur = mysql.connection.cursor()
     query = """
-    select * from employee
+    SELECT * FROM employee
     """
     cur.execute(query)
     data = cur.fetchall()
-    cur.close
+    cur.close()
 
     return make_response(jsonify(data), 200)
 
+@app.route("/employee/<int:ssn>",methods=["GET"])
+def get_employee_by_ssn(ssn):
+    cur = mysql.connection.cursor()
+    query = """
+    SELECT * FROM employee where ssn = {}
+    """.format(ssn)
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Test.py
@@ -0,0 +1,23 @@
+import unittest
+import warnings
+from Api import app
+
+class MyTests(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+    def test_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
+
+    def test_getemployee_by_ssn(self):
+        response = self.app.get("/employee/123456789")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("John" in response.data.decode())
+
+if __name__ == "__main__":
+    unittest.main()
