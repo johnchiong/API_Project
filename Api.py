a93c6afac2dd1b61a4856cc0fa96c20583d3f527
johnchiong <152459351+johnchiong@users.noreply.github.com>
2024-05-24 21:44:40 +0800

    updated

--- Api.py
+++ Api.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -34,6 +34,22 @@ def get_employee():
 def get_employee_by_ssn(ssn):
     data = data_fetch("SELECT * FROM employee WHERE ssn = %s", (ssn,))
     return make_response(jsonify(data), 200)
+
+@app.route("/employee", methods=["POST"])
+def add_employee():
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    fname =info["first_name"]
+    lname = info["last_name"]
+    cur.execute(
+        """ INSERT INTO employee (first_name, last_name) Value (%s, %s)""", (fname, lname),
+    )
+    mysql.connection.commit()
+    print("row(s) affected :{}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(jsonify({"message": "employee added succesfully", "rows_affected": cur.rowcount}), 200)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
