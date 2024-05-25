223c2b6ee62cb5caee0496fc3e83150b36d55046
johnchiong <152459351+johnchiong@users.noreply.github.com>
2024-05-25 16:07:56 +0800

    Update Api.py

--- Api.py
+++ Api.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify, request, render_template_string
+from flask import Flask, make_response, jsonify, request, render_template_string, redirect, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -114,20 +114,74 @@ def get_employee():
 </html>
 """, employees=employees)
 
-@app.route("/employee", methods=["POST"])
+@app.route("/add_employee", methods=["GET", "POST"])
 def add_employee():
-    cur = mysql.connection.cursor()
-    info = request.get_json()
-    fname =info["first_name"]
-    lname = info["last_name"]
-    cur.execute(
-        """ INSERT INTO employee (first_name, last_name) Value (%s, %s)""", (fname, lname),
-    )
-    mysql.connection.commit()
-    print("row(s) affected :{}".format(cur.rowcount))
-    rows_affected = cur.rowcount
-    cur.close()
-    return make_response(jsonify({"message": "employee added successfully", "rows_affected": cur.rowcount}), 200)
+    if request.method == "POST":
+        fname = request.form["fname"]
+        minit = request.form["minit"]
+        lname = request.form["lname"]
+        address = request.form["address"]
+        bdate = request.form["bdate"]
+        dl_id = request.form["dl_id"]
+        salary = request.form["salary"]
+        sex = request.form["sex"]
+        super_ssn = request.form["super_ssn"]
+        ssn = request.form["ssn"]
+
+        query = """
+        INSERT INTO employee (Fname, Minit, Lname, Address, Bdate, DL_id, Salary, Sex, Super_ssn, ssn)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        values = (fname, minit, lname, address, bdate, dl_id, salary, sex, super_ssn, ssn)
+        cur = mysql.connection.cursor()
+        cur.execute(query, values)
+        mysql.connection.commit()
+        cur.close()
+        
+        return redirect(url_for("get_employee"))
+    
+    return render_template_string("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Employee</title>
+</head>
+<body>
+    <h1>Add an Employee</h1>
+    <form action="/add_employee" method="post">
+        <label for="fname">First Name:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+        <label for="minit">Middle Initial:</label><br>
+        <input type="text" id="minit" name="minit"><br>
+        <label for="lname">Last Name:</label><br>
+        <input type="text" id="lname" name="lname"><br>
+        <label for="address">Address:</label><br>
+        <input type="text" id="address" name="address"><br>
+        <label for="bdate">Birthdate:</label><br>
+        <input type="date" id="bdate" name="bdate"><br>
+        <label for="dl_id">Department Location ID:</label><br>
+        <input type="text" id="dl_id" name="dl_id"><br>
+        <label for="salary">Salary:</label><br>
+        <input type="number" id="salary" name="salary"><br>
+        <label for="sex">Sex:</label><br>
+        <input type="text" id="sex" name="sex"><br>
+        <label for="super_ssn">Supervisor SSN:</label><br>
+        <input type="text" id="super_ssn" name="super_ssn"><br>
+        <label for="ssn">SSN:</label><br>
+        <input type="text" id="ssn" name="ssn"><br>
+        <input type="submit" value="Add Employee">
+    </form>
+    <button id="return-home-btn">Return to Home</button>
+    <script>
+        document.getElementById("return-home-btn").addEventListener("click", function() {
+            window.location.href = "/";
+        });
+    </script>
+</body>
+</html>
+""")
 
 @app.route("/employee/<int:ssn>", methods=["Put"])
 def update_employee(ssn):
